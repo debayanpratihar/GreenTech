d555e29f5426bcbc23566a7d2e267ab778607dc3
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2024-12-20 00:50:41 +0530

    build complete

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,23 @@
-import withPWA from "next-pwa";
+import withPWAInit from "next-pwa";
+const withPWA = withPWAInit({
+    dest: 'public'
+})
 
-const isProd = process.env.NODE_ENV === "production";
 
-export default isProd
-    ? withPWA({
-          dest: 'public',
-          register: true,
-          skipWaiting: true,
-          pwa: {
-              dest: 'public',
-              register: true,
-              skipWaiting: true,
-          },
-          images: {
-              remotePatterns: [
-                  {
-                      protocol: "https",
-                      hostname: "**",
-                  },
-              ],
-          },
-          experimental: {
-              missingSuspenseWithCSRBailout: false,
-          },
-      })
-    : {
-          images: {
-              remotePatterns: [
-                  {
-                      protocol: "https",
-                      hostname: "**",
-                  },
-              ],
-          },
-          experimental: {
-              missingSuspenseWithCSRBailout: false,
-          },
-      };
+export default withPWA({
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    }, experimental: {
+        missingSuspenseWithCSRBailout: false,
+    }
+})
